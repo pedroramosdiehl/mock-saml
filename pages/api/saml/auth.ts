0a5ab652ed1f3882ffb92a9616e7646c77f156d0
Pedro Diehl <pedrodiehl@ufg.br>
2024-02-22 22:33:52 -0300

    add groups and defautl group

--- pages/api/saml/auth.ts
+++ pages/api/saml/auth.ts
@@ -1,27 +1,32 @@
+import saml from '@boxyhq/saml20';
 import { createHash } from 'crypto';
+import { getEntityId } from 'lib/entity-id';
 import config from 'lib/env';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { User } from 'types';
-import saml from '@boxyhq/saml20';
-import { getEntityId } from 'lib/entity-id';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { email, audience, acsUrl, id, relayState } = req.body;
 
-    if (!email.endsWith('@example.com') && !email.endsWith('@example.org')) {
+    if (!email.endsWith('@example.com')) {
+      console.log('Denied:', email);
       res.status(403).send(`${email} denied access`);
     }
 
     const userId = createHash('sha256').update(email).digest('hex');
     const userName = email.split('@')[0];
 
+
     const user: User = {
       id: userId,
       email,
       firstName: userName,
       lastName: userName,
+      groups: ['Admin'],
     };
+
+    console.log('User:', user);
 
     const xmlSigned = await saml.createSAMLResponse({
       issuer: getEntityId(config.entityId, req.query.namespace as any),
